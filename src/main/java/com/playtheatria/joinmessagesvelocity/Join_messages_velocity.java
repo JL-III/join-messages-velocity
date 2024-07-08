f24b72aac26f4db6e5f8f2499371403ae446379a
JL-III <jesselopez1162@gmail.com>
2024-07-08 08:33:26 -0500

    add formatting

--- src/main/java/com/playtheatria/joinmessagesvelocity/Join_messages_velocity.java
+++ src/main/java/com/playtheatria/joinmessagesvelocity/Join_messages_velocity.java
@@ -9,6 +9,8 @@ import com.velocitypowered.api.plugin.Plugin;
 import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.format.TextColor;
 import org.slf4j.Logger;
 
 @Plugin(
@@ -28,16 +30,17 @@ public class Join_messages_velocity {
     }
 
     @Subscribe
-    public void onProxyInitialization(ProxyInitializeEvent event) {
-    }
-
-    @Subscribe
     public void onPostLoginEvent(PostLoginEvent event) {
         if (event.getPlayer().hasPermission("join-messages-velocity.login.silent")) return;
         logger.info("{} has joined and was detected by join-messages-velocity", event.getPlayer().getUsername());
         for (Player player : server.getAllPlayers()) {
             if (!player.hasPermission("join-messages-velocity.login.ignore")) {
-                player.sendMessage(Component.text("[+] " + event.getPlayer().getUsername()));
+                player.sendMessage(
+                        Component.text("[", NamedTextColor.DARK_GRAY).append(
+                        Component.text("+", NamedTextColor.GREEN).append(
+                        Component.text("] ", NamedTextColor.DARK_GRAY).append(
+                        Component.text(event.getPlayer().getUsername(), NamedTextColor.DARK_GRAY))))
+                );
             }
         }
     }
@@ -48,8 +51,12 @@ public class Join_messages_velocity {
         logger.info("{} has disconnected and was detected by join-messages-velocity", event.getPlayer().getUsername());
         for (Player player : server.getAllPlayers()) {
             if (!player.hasPermission("join-messages-velocity.logout.ignore")) {
-                player.sendMessage(Component.text("[-] " + event.getPlayer().getUsername()));
-            }
+                player.sendMessage(
+                        Component.text("[", NamedTextColor.DARK_GRAY).append(
+                                Component.text("-", NamedTextColor.DARK_AQUA).append(
+                                        Component.text("] ", NamedTextColor.DARK_GRAY).append(
+                                                Component.text(event.getPlayer().getUsername(), NamedTextColor.DARK_GRAY))))
+                );            }
         }
     }
 }
