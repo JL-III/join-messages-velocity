fc8115c1e67b253238b633eb49d2412a80026f91
JL-III <jesselopez1162@gmail.com>
2024-07-10 11:49:44 -0500

    fix bug

--- src/main/java/com/playtheatria/joinmessagesvelocity/Join_messages_velocity.java
+++ src/main/java/com/playtheatria/joinmessagesvelocity/Join_messages_velocity.java
@@ -9,6 +9,7 @@ import com.velocitypowered.api.proxy.Player;
 import com.velocitypowered.api.proxy.ProxyServer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.slf4j.Logger;
 
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
@@ -33,14 +34,14 @@ public class Join_messages_velocity {
     @Subscribe
     public void onPostLoginEvent(PostLoginEvent event) {
         if (event.getPlayer().hasPermission("join-messages-velocity.login.silent")) return;
-        for (Player player : server.getAllPlayers()) {
-            if (player.hasPermission("join-messages-velocity.login.ignore")) continue;
+        for (Player playerToNotify : server.getAllPlayers()) {
+            if (playerToNotify.hasPermission("join-messages-velocity.login.ignore")) continue;
             server.getScheduler()
                     .buildTask(this, () -> {
                         // abort immediately since the player is no longer online
-                        if (!player.isActive()) return;
+                        if (!event.getPlayer().isActive()) return;
                         sendPlayerMessage(
-                                player,
+                                playerToNotify,
                                 event.getPlayer().getUsername(),
                                 MessageType.CONNECT,
                                 NamedTextColor.DARK_GRAY,
@@ -48,8 +49,8 @@ public class Join_messages_velocity {
                                 NamedTextColor.GRAY
                         );
                         // check if the player has silenced logout messages, if so, there's no need to add them to the disconnect list
-                        if (player.hasPermission("join-messages-velocity.logout.silent")) return;
-                        notifyDisconnectList.add(player);
+                        if (event.getPlayer().hasPermission("join-messages-velocity.logout.silent")) return;
+                        notifyDisconnectList.add(event.getPlayer());
                     })
                     .delay(3L, TimeUnit.SECONDS) // this time is pretty arbitrary, we just want to give enough time for other processes to do their thing
                     .schedule();
